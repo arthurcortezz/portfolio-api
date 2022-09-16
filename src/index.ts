066204b0835e30bf18b0ecadd9ad60ba390945b6
Arthur Cortez <arthurcortez2@gmail.com>
2022-09-16 17:40:49 -0300

    comment route added

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,59 @@
-const http = require("http");
-const hostName = "192.168.0.9";
-const port = 3001;
+import cors from "cors";
 
-console.log("server running");
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader("content-Type", "text/plain");
-//   res.end("Hello World");
-// });
-// server.listen(port, hostName, () => {
-// });
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+const SECRET = "tuca1";
+
+//read json / midlewares
+var corsOptions = { origin: "http://localhost:3000", credentials: true };
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+//port
+app.listen(3001);
+
+//routes
+app.post("/comment", (req: any, res: any) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  const object: any = {
+    name: req.body.name,
+    reason: req.body.reason,
+    message: req.body.message,
+  };
+  console.log("🚀 ~ file: index.ts ~ line 35 ~ app.post ~ object", object);
+  return res.json({ name: "Arthur" });
+});
+
+app.post("/login/", (req: any, res: any) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3001/comment/"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  if (req.body.user === "tuca" && req.body.pass === "123") {
+    const token = jwt.sign({ idUser: 1 }, SECRET, { expiresIn: 500 });
+    console.log("Login");
+    return res.json({ auth: true, token });
+  }
+  res.status(401).end();
+});
--- src/routes/comment.ts
+++ src/routes/comment.ts
@@ -3,4 +3,5 @@ import { CommentController } from "../controllers";
 
 const router = Router();
 const ROUTER_BASE = "/comment";
+
 router.post(`${ROUTER_BASE}/createComment`, CommentController.newComment);
