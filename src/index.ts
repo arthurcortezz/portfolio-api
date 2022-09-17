205f985c56eb889b04b720dcfc8826bf2681aac4
Arthur Cortez <arthurcortez2@gmail.com>
2022-09-16 21:14:53 -0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 const SECRET = "tuca1";
 
 //read json / midlewares
-var corsOptions = { origin: "http://localhost:3000", credentials: true };
+var corsOptions = { origin: "*", credentials: true };
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,39 +17,15 @@ app.listen(3001);
 
 //routes
 app.post("/comment", (req: any, res: any) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
   const object: any = {
     name: req.body.name,
     reason: req.body.reason,
     message: req.body.message,
   };
-  console.log("🚀 ~ file: index.ts ~ line 35 ~ app.post ~ object", object);
-  return res.json({ name: "Arthur" });
+  return res.json({ object: object });
 });
 
 app.post("/login/", (req: any, res: any) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3001/comment/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
   if (req.body.user === "tuca" && req.body.pass === "123") {
     const token = jwt.sign({ idUser: 1 }, SECRET, { expiresIn: 500 });
     console.log("Login");
