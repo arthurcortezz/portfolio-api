e7402531553bcdcb3adc05d1c67d5909d7962bd0
Arthur Cortez <arthurcortez2@gmail.com>
2022-11-10 14:20:20 -0300

    removendo comentários

--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -7,21 +7,11 @@ const notificacao = new NotificationController();
 const crypto = new Cryptography();
 router.post(
   "/notification/saveSubscription",
-  // cripto.descriptografarRequest,
-  // ensureAuthenticated,
-  // authorization,
-  // validarQuery(notificacaoPush),
   notificacao.newSubscription
-  // crypto.criptografar
 );
 router.post(
   "/notification/notificationPush",
-  // cripto.descriptografarRequest,
-  // ensureAuthenticated,
-  // authorization,
-  // validarQuery(notificacaoPush),
   notificacao.notificacaoPush
-  // crypto.criptografar
 );
 
 export default router;
